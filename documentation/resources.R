fc2605c0a828469750e6e8b191c2f0059947d683
maddocent <marc.teunis@hu.nl>
2017-01-18 14:04:32 +0000

    added documentation and slides

--- documentation/resources.R
+++
@@ -1,8 +0,0 @@
-# Tidy Data article and demo
-ftp://cran.r-project.org/pub/R/web/packages/tidyr/vignettes/tidy-data.html
-
-# Tidy Data article:
-http://vita.had.co.nz/papers/tidy-data.html
-
-# R for Data Science
-http://r4ds.had.co.nz/
